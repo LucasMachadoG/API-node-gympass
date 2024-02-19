2cf69dd0c9c4927a596f9ff512239e70b2bdf8df
Lucas Gaelzer Machado <lucasgaelzermachado@gmail.com>
2024-02-19 14:39:49 -0300

    global error handler

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,9 +1,30 @@
 import fastify from "fastify";
 import { userRoutes } from "../http/routes/user.routes";
+import { ZodError } from "zod";
+import { env } from "../env";
 
 export const app = fastify()
 
 app.register(userRoutes, {
   prefix: '/users'
+})
+
+app.setErrorHandler((error, request, reply) => {
+  if(error instanceof ZodError){
+    return reply.status(400).send({
+      message: 'Validation error.',
+      issues: error.format()
+    })
+  }
+
+  if(env.NODE_ENV !== 'production'){
+    console.error(error)
+  } else {
+    
+  }
+
+  return reply.status(500).send({
+    message: 'Internal server error.'
+  })
 })
 
